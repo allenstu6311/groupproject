4e5095e7f5a152246300ff1a02ed3f22aa468295
FanWeny <z04wj06y7@gmail.com>
2022-09-27 09:29:49 +0800

    fix about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,23 +1,20 @@
 <template>
-  <Header />
-    <About />
-  <Footer />
-      
-      
-<router-view/>
-
+  	<Header />
+	<About />
+  	<Footer />
+<!-- <router-view/> -->
 </template>
 
 <script>
 import Header from "@/components/Header.vue"
 import Footer from "@/components/Footer.vue"
 import About from "@/components/About.vue"
-    export default{
 
-        components:{
-            Header,
-            Footer,
-            About
-        }
+export default{
+    components:{
+        Header,
+        Footer,
+        About
     }
+}
 </script>

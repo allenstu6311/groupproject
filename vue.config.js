ce9aab8039182ffb05e53bb4f6dd9d12f983e42a
sandranii <niyouu63@gmail.com>
2022-09-30 16:47:53 +0800

    fix vue.config.js: 修改上線路徑

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  //影響index.html取js的取得路徑
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/cgd102/g2'
+    : '/',
+
 })

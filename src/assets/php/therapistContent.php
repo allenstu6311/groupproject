57b5035db69259138087fdf8edab3bdeb7ae591e
FanWeny <z04wj06y7@gmail.com>
2022-09-30 22:58:42 +0800

    fix therapist vue.js -> php

--- src/assets/php/therapistContent.php
+++ src/assets/php/therapistContent.php
@@ -6,28 +6,24 @@ try{
 
     require_once("../../connect_cgd102g2.php");
 
-    $sql = "select * from `therapist` where THERAPIST_ID = 1";
-    $therapist = $pdo->query($sql); //先編譯好
-	// $therapist->bindValue(":THERAPIST_ID", $THERAPIST_ID); //代入資料
-	// $therapist->execute();//執行之
+    $sql = "select * from therapist";
+    $therapist = $pdo->query($sql);
 
     if($therapist->rowCount()==0){
         echo "無按摩師資料";
         // exit();
     }else{
-        $therapists = $therapist->fetch(PDO::FETCH_ASSOC);
-        $data=[];
+        $therapists = $therapist->fetchAll(PDO::FETCH_ASSOC);
+        $data = [];
 
-        foreach($therapists as $i=> $page){
+        foreach($therapists as $i => $page){
         
         ?>
-        
         <?php
-        
-            $data[]=$page;
+        $data[] = $page;
         }
+
         echo json_encode($data);
-        
         ?>
         <?php
     }

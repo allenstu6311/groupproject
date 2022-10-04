ae74f2f4d73fb0c4d8a6de0750eb9589d750a19d
KangChinHao <t8206123@gmail.com>
2022-10-04 14:20:20 +0800

    fix:註冊會員的判斷 add:會員登入(還不完整)

---
+++ src/assets/phps/login.php
@@ -0,0 +1,44 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header("Content-Type:application/json;charset=utf-8");
+session_start();
+try{
+    require_once("../../connect_cgd102g2.php");
+    $findExistSql = "SELECT * 
+            FROM member 
+            WHERE MEM_ACCOUNT=:ACCOUNT
+            AND MEM_PSW=:PSW";
+    $existAccount = $pdo -> prepare($findExistSql);
+    $existAccount->bindValue(":ACCOUNT",$_POST["account"]);
+    $existAccount->bindValue(":PSW",$_POST["password"]);
+    $existAccount->execute();
+    
+    if( $existAccount->rowCount() == 0 ){ //找不到
+        echo "帳號密碼有誤";
+    }else{ //找得到
+      //取回會員資料，存入session
+        $memData = $existAccount->fetch(PDO::FETCH_ASSOC);
+        $_SESSION["memName"] = $memData["MEM_NAME"];
+        $_SESSION["memAccount"] = $memData["MEM_ACCOUNT"];
+        $_SESSION["memPsw"] = $memData["MEM_PSW"];
+        $_SESSION["memBirthday"] = $memData["MEM_BIRTHDAY"];
+        $_SESSION["memAddress"] = $memData["MEM_ADDRESS"];
+        $_SESSION["memPhone"] = $memData["MEM_PHONE"];
+        $_SESSION["memLocalCall"] = $memData["MEM_LOCALCALL"];
+        $_SESSION["memEmail"] = $memData["MEM_EMAIL"];
+        //送出登入者的資料
+        $result = ["memName"=>$_SESSION["memName"],
+                   "memAccount"=>$_SESSION["memAccount"],
+                   "memPsw"=>$_SESSION["memPsw"],
+                   "memBirthday"=>$_SESSION["memBirthday"],
+                   "memAddress"=>$_SESSION["memAddress"],
+                   "memPhone"=>$_SESSION["memPhone"],
+                   "memLocalCall"=>$_SESSION["memLocalCall"],
+                   "memEmail"=>$_SESSION["memEmail"],
+                  ];
+        echo json_encode($result);
+    }	
+  }catch(PDOException $e){
+    echo $e->getMessage();
+  }
+?>

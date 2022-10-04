7e2bb9e273f5a113441c583d85d8efd7e3ba3b7d
sandranii <niyouu63@gmail.com>
2022-10-04 12:07:35 +0800

    add reservation.php, fix Reservation.vue

---
+++ src/assets/phps/reservation.php
@@ -0,0 +1,36 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header("Content-Type:application/json;charset=utf-8");
+
+try{
+
+    require_once("../../connect_cgd102g2.php");
+
+    $sql = "SELECT * FROM MSG";
+    $msg = $pdo->query($sql);
+
+    if($msg->rowCount()==0){
+        echo "無資料";
+        // exit();
+    }else{
+        $msgs = $msg->fetchAll(PDO::FETCH_ASSOC);
+        $data = [];
+
+        foreach($msgs as $i => $page){
+        
+        ?>
+        <?php
+        $data[] = $page;
+        }
+
+        echo json_encode($data);
+        ?>
+        <?php
+    }
+}catch (Exception $e) {
+        echo "錯誤行號 : ", $e->getLine(), "<br>";
+        echo "錯誤原因 : ", $e->getMessage(), "<br>";
+        //echo "系統暫時不能正常運行，請稍後再試<br>";	
+}
+?>
+
--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -32,39 +32,39 @@
             :key="index"
             :class="{ reverse: index % 2 === 1, btnLeft: index%2 !==1}"
           >
-            <div class="msg_card row">
-              <div class="msg_card_txt col col-12 col-md-12 col-xl-8">
+            <div class="msg_card row"  >
+              <div class="msg_card_txt col col-12 col-md-12 col-xl-8" >
                 <h3 class="name item_title">
-                  {{ msgCard.name }}
+                  {{ msgCard.MSG_NAME }}
                 </h3>
                 <div class="intro">
-                  {{ msgCard.intro }}
+                  {{ msgCard.MSG_INTRO }}
                 </div>
                 <div class="price_form">
                   <div class="long_time">
                     <div class="long_time_price">
                       <span class="timespan timespan_1">
-                        {{ msgCard.timespan_1 }}分鐘 </span
+                        {{ msgCard.MSG_TIMESPAN_1 }}分鐘 </span
                       >/
                       <span class="price price_1">
-                        NT${{ msgCard.price_1 }}
+                        NT${{ msgCard.MSG_PRICE_1 }}
                       </span>
                     </div>
                     <p class="rec rec_1">
-                      {{ msgCard.rec_1 }}
+                      {{ msgCard.MSG_REC_1 }}
                     </p>
                   </div>
                   <div class="short_time">
                     <div class="short_time_price">
                       <span class="timespan timespan_2">
-                        {{ msgCard.timespan_2 }}分鐘 </span
+                        {{ msgCard.MSG_TIMESPAN_2 }}分鐘 </span
                       >/
                       <span class="price price_2">
-                        NT${{ msgCard.price_2 }}
+                        NT${{ msgCard.MSG_PRICE_2 }}
                       </span>
                     </div>
                     <p class="rec rec_2">
-                      {{ msgCard.rec_2 }}
+                      {{ msgCard.MSG_REC_2 }}
                     </p>
                   </div>
                 </div>
@@ -74,7 +74,7 @@
               </div>
               <div class="pic_ctnr msg_card_pic col col-12 col-md-8 col-xl-4">
                 <img
-                  :src="require(`@/assets/images/${msgCard.pic}`)"
+                  :src="require(`@/assets/images/${msgCard.MSG_PIC}`)"
                   :alt="msgCard.pic_alt"
                 />
               </div>
@@ -85,66 +85,32 @@
     </main>
   </div>
 </template>
+
 <script>
-export default {
-  props: {},
-  data() {
-    return {
-      msgCardList: [
-        {
-          name: "全身穴道按摩",
-          intro:
-            "經典穴道按摩課程，經過經絡及淋巴循環，釋放緊繃、痠痛的肌肉；深層紓壓舒緩身心，特別推薦給工作壓力大、肌肉僵硬容易痠痛者，例如久坐辦公室或放鬆運動過後的身體。",
-          timespan_1: 120,
-          price_1: 2400,
-          rec_1: "適合平常沒有按摩習慣或身體很多部位都很緊繃者​",
-          timespan_2: 60,
-          price_2: 1600,
-          rec_2: "適合平常有按摩習慣者​",
-          pic: "resv1.jpg",
-          pic_alt: "全身穴道按摩照片",
+    export default {
+        props: {
+            msg: String,
+            msgCard: String,
         },
-        {
-          name: "精油深層放鬆",
-          intro:
-            "提供珍貴漢方中藥草精油，在純手技按摩手法外，利用不同精油的植物特性加強放鬆。以中式推拿舒展筋骨，西式芳療順淋巴，從身體到情緒，把負面疲勞一一帶走，達到身心排毒的功效。",
-          timespan_1: 120,
-          price_1: 2400,
-          rec_1: "適合平常沒有按摩習慣或身體很多部位都很緊繃者​",
-          timespan_2: 60,
-          price_2: 1600,
-          rec_2: "適合平常有按摩習慣者​",
-          pic: "resv2.jpg",
-          pic_alt: "精油深層放鬆照片",
+        data() {
+            return {
+                msgCardList: [],
+            }
         },
-        {
-          name: "東方經絡指壓",
-          intro:
-            "找到引發痠痛的源頭部位，根據每個人的情況，靈活運用傳統東方指壓技法，推順氣結並深入經絡，精準的穴位指壓能快速緩解筋肉痠痛，真正順暢筋絡，達到放鬆舒緩，或更深度的修復。",
-          timespan_1: 120,
-          price_1: 2200,
-          rec_1: "適合平常沒有按摩習慣或身體很多部位都很緊繃者​",
-          timespan_2: 60,
-          price_2: 1500,
-          rec_2: "適合平常有按摩習慣者​",
-          pic: "resv3.jpg",
-          pic_alt: "東方經絡指壓照片",
+        created(){
+            this.getDataFromApi(); // 在建立 Vue.js 模板時順帶執行這個參數
         },
-        {
-          name: "足部舒壓按摩",
-          intro:
-            "按摩前先讓每位客人沐足，雙腳浸泡在加了薰衣草沐浴鹽的溫水裡。接著透過專業手法，舒緩您小腿肌肉的緊繃，並依照刺激穴道原理，刺激腳底各部位反射區來活絡循環，鬆解腿部疲勞感，有效去除小腿水腫。",
-          timespan_1: 120,
-          price_1: 1800,
-          rec_1: "適合平常沒有按摩習慣或身體很多部位都很緊繃者​",
-          timespan_2: 60,
-          price_2: 1200,
-          rec_2: "適合平常有按摩習慣者​",
-          pic: "resv4.jpg",
-          pic_alt: "足部舒壓按摩照片",
-        },
-      ],
-    };
-  },
-};
+        methods:{
+            async getDataFromApi() {
+                var url = 'http://localhost/CGD102_G2/src/assets/phps/reservation.php'
+                let getData = async(url) => {
+                    let response = await fetch(url); // await: 這行的 await 執行完才會執行下一個 await
+                    let JSON =  response.json();
+                    this.msgCardList = await JSON; // php 抓取回來的資料存取在預設好的參數裡
+                }
+                await getData(url); // 觸發 getData 的匿名 function 內容 ==> 89 ~ 91 行的內容
+                console.log(this.msgCardList);
+            }
+        }
+    }
 </script>

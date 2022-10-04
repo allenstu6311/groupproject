14e51a6351ab5821faaf3c86ade7fef121713c0d
sandranii <niyouu63@gmail.com>
2022-10-04 23:14:23 +0800

    fix public/api/reservation.php: require_once 上線路徑

--- public/api/reservation.php
+++ public/api/reservation.php
@@ -4,7 +4,8 @@ header("Content-Type:application/json;charset=utf-8");
 
 try{
 
-    require_once("../../src/connect_cgd102g2.php");
+    // require_once("../../src/connect_cgd102g2.php");
+    require_once("../connect_cgd102g2.php");//上線路徑
 
     $sql = "SELECT * FROM MSG";
     $msg = $pdo->query($sql);

ccb01192db44c1d33810f7f58dd0bc2af37011c3
YenTzuChien <sypss92062@gmail.com>
2022-10-05 14:15:19 +0800

    fix favicon.ico跟網頁名稱 header會員登入後狀態

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,31 +2,37 @@
    <header class="page_header">
     <nav>
         <input type="checkbox" id="check">
-        <label for="check">
+        <label for="check" class="hamburger">
             <span class="line1"></span>
             <span class="line2"></span>
             <span class="line3"></span>
         </label> 
         <router-link to="/home"   class="header_logo"><img src="../assets/images/headerLogo.png" alt=""></router-link>
     
-        <ul>
+        <ul class="navList">
             <li><router-link to="/about">關於我們</router-link></li>
             <li><router-link to="/reservation">預約按摩</router-link></li>
             <li><router-link to="/ProductList">商品專區</router-link></li>
-            <!-- <a href="../index.html" class="header_logo"><img src="../images/headerLogo.png" alt=""></a> -->
             <li><router-link to="/MassageSchool">按摩小學堂</router-link></li>
             <li><router-link to="/message">常見問題</router-link></li>
         </ul>
         <div class="header_icon">
-            <span class="header_loginMember"><img src="../assets/images/loginHead.png" alt=""></span>
+            <!--這是已登入的大頭--->
+            <input type="checkbox" id="check2">
+            <label for="check2">
+                <span class="header_loginMember"><img src="../assets/images/loginHead.png" alt=""></span>
+            </label>
 
+            <!--這是大頭下拉選單--->
             <ul class="headerLogin">
-                <li><router-link to="/MemCenterView">會員中心</router-link></li>
+                <li><router-link to="/MemCenter">會員中心</router-link></li>
                 <li><router-link to="">登出</router-link></li>
             </ul>
 
+            <!--這是未登入的大頭--->
             <span class="header_member"><router-link to="/MemLogin"><img src="../assets/images/headerMember.png" alt=""></router-link></span>
 
+            <!--這是購物車--->
             <span class="header_shopping_cart"><router-link to="/cart"><img src="../assets/images/headerShoppinCart.png" alt=""></router-link></span>
         </div>  
         

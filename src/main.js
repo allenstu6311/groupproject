0075ce484fdbf4c7cc6a88bcf49268ae99fd059b
sandranii <niyouu63@gmail.com>
2022-10-07 15:50:44 +0800

    fix main.js, package.json

--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
 import { createApp } from 'vue'
 import axios from "axios"
-import VueAxios from 'vue-axios'
 import App from './App.vue'
-// import Header from "@/components/Header.vue"
-// import Footer from "@/components/Footer.vue"
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-// import 'view-ui-plus/dist/styles/viewuiplus.css'
-// import "bootstrap/dist/css/bootstrap.css"
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
 
 
 createApp(App).use(store)
                 .use(router)
-                .use(VueAxios,axios)
+                .use(axios)
                 .mount('#app')
-// .ues(Header).use(Footer)

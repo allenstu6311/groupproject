1890ae164478bbf1b7772cc3d580e16f680e7379
sandranii <niyouu63@gmail.com>
2022-10-07 16:30:08 +0800

    Revert "fix main.js, package.json"
    This reverts commit 0075ce484fdbf4c7cc6a88bcf49268ae99fd059b.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
 import axios from "axios"
+import VueAxios from 'vue-axios'
 import App from './App.vue'
+// import Header from "@/components/Header.vue"
+// import Footer from "@/components/Footer.vue"
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+// import 'view-ui-plus/dist/styles/viewuiplus.css'
+// import "bootstrap/dist/css/bootstrap.css"
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 
 createApp(App).use(store)
                 .use(router)
-                .use(axios)
+                .use(VueAxios,axios)
                 .mount('#app')
+// .ues(Header).use(Footer)

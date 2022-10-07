204e66d6cd495089865d7b6fc99899933ea479ba
allen <stuallen6311@gmail.com>
2022-10-07 21:48:36 +0800

    fix cart

--- public/api/addOn.php
+++ public/api/addOn.php
@@ -3,8 +3,8 @@
 header('Access-Control-Allow-Origin:*');
 header("Content-Type:application/json;charset=utf-8");
 
-// require_once("../../src/connect_cgd102g2.php"); //開發路徑
-require_once("../connect_cgd102g2.php");//上線路徑
+require_once("../../src/connect_cgd102g2.php"); //開發路徑
+// require_once("../connect_cgd102g2.php");//上線路徑
 
 $sql = "select * from product";
 $product = $pdo->query($sql);
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,52 +1,46 @@
 <template>
-
-     <div class="background-pic">
-        <img src="../assets/images/bcgFlower.png" alt="">
-    </div>
-    <ProductCart  :getProduct="temporary"
-                    @cart-message="cartInformation"/>
-    <ShoppingCar   @product-info="productInfo"
-                    :checkCar ="carSend"/>
-
+  <div class="background-pic">
+    <img src="../assets/images/bcgFlower.png" alt="" />
+  </div>
+  <ProductCart :getProduct="temporary" @cart-message="cartInformation" />
+  <ShoppingCar @product-info="productInfo" :checkCar="carSend" />
 </template>
 
 <script>
+import ShoppingCar from "@/components/ShoppingCar.vue";
+import ProductCart from "@/components/ProductCart.vue";
 
-import ShoppingCar from "@/components/ShoppingCar.vue"
-import ProductCart from "@/components/ProductCart.vue"
- 
 export default {
-    components:{
-      
-        ShoppingCar,
-        ProductCart,
+  components: {
+    ShoppingCar,
+    ProductCart,
+  },
+  data() {
+    return {
+      temporary: [],
+      carSend: [],
+    };
+  },
+  methods: {
+    productInfo(val) {
+      // console.log("val",val)
+      this.temporary.push(val);
     },
-    data(){
-        return{
-            temporary:[],
-            carSend:[],
-            
-        }
+    cartInformation(val) {
+      console.log("val", val);
+      this.carSend = val;
     },
-    methods:{
-        productInfo(val){
-            // console.log("val",val)
-            this.temporary.push(val)
-        },
-        cartInformation(val){
-            console.log("val",val)
-            this.carSend = val
-        }
-    },
-    created(){
-        // console.log("臨時",this.temporary)
-        this.productInfo()
-    },
-    watch:{
-       
-    },
-    updated() {
-        // console.log("t",this.temporary)
-    },
-}
+  },
+  created() {
+    this.productInfo();
+    let members = sessionStorage.getItem("member");
+    this.member = JSON.parse(members);
+    console.log("mem", this.member);
+    
+  },
+  watch: {},
+  updated() {
+    // console.log("t",this.temporary)
+  },
+};
 </script>

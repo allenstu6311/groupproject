eb5362b03a0f8af1001af2083fe180de281bb85a
SheRiLi99 <sheri4959520@gmail.com>
2022-10-11 13:42:31 +0800

    fix getcoupon路徑

--- src/components/BackCoupon.vue
+++ src/components/BackCoupon.vue
@@ -59,14 +59,9 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-// import "bootstrap/scss/bootstrap.scss";
-    import {BASE_URL} from '@/assets/js/common.js'
-=======
-import "bootstrap/scss/bootstrap.scss";
 
->>>>>>> sherili
-    //const BASE_URL = process.env.NODE_ENV === 'production'? '/cgd102/g2': '..' //上線用
+    import {BASE_URL} from '@/assets/js/common.js'
+
 
     export default {
         props: {

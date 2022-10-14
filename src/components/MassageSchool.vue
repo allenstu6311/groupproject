4b149fdacdc51c6c26b4d118137fbbdd0170eb64
SheRiLi99 <sheri4959520@gmail.com>
2022-10-14 11:40:51 +0800

    fix swiper console

--- src/components/MassageSchool.vue
+++ src/components/MassageSchool.vue
@@ -394,10 +394,10 @@
         },
         setup() {
             const onSwiper = (swiper) => {
-                console.log(swiper);
+                
             };
             const onSlideChange = () => {
-                console.log('slide change');
+
             };
             return {
                 onSwiper,

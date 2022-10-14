f6e6f08818271ff29e853aeb4431bbe64e596b3b
sandranii <niyouu63@gmail.com>
2022-10-14 10:23:14 +0800

    fix memberCenterResv.php: sql

--- public/api/memberCenterResv.php
+++ public/api/memberCenterResv.php
@@ -13,11 +13,23 @@ function getMemberId($mem_id){
 //   ORDER BY POD.PROD_ORDERS_DATE;";
 
 
-$sql="SELECT R.RESV_ID,R.RESV_DATE,M.MEM_NAME,R.RESV_STATUS
-        FROM RESV R JOIN MEMBER M 
+// $sql="SELECT R.RESV_ID,R.RESV_DATE,M.MEM_NAME,R.RESV_STATUS
+//         FROM RESV R JOIN MEMBER M 
+//         ON R.MEM_ID=M.MEM_ID
+//         WHERE R.MEM_ID={$mem_id}
+//         ORDER BY R.RESV_DATE;";
+
+
+$sql="SELECT R.RESV_DATE, MSG.MSG_NAME, T.THERAPIST_NAME, MSG.MSG_PRICE_2, R.RESV_TIME_START, MSG.MSG_TIMESPAN_2
+        FROM RESV R 
+        JOIN MEMBER M 
         ON R.MEM_ID=M.MEM_ID
+		JOIN THERAPIST T 
+        ON R.THERAPIST_ID=T.THERAPIST_ID
+		JOIN MSG
+        ON R.MSG_ID=MSG.MSG_ID
         WHERE R.MEM_ID={$mem_id}
-        ORDER BY R.RESV_DATE;";
+        ;";
 
   
   $info = $pdo->query($sql);

f9fe016e864da5149cbc02de7d2ce4599406f4ca
sandranii <niyouu63@gmail.com>
2022-10-22 22:36:32 +0800

    fix Reservation.vue: 刪除多餘註解

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -41,19 +41,6 @@
                   {{ msgCard.MSG_INTRO }}
                 </div>
                 <div class="price_form">
-                  <!-- <div class="long_time">
-                    <div class="long_time_price">
-                      <span class="timespan timespan_1">
-                        {{ msgCard.MSG_TIMESPAN_1 }}分鐘 </span
-                      >/
-                      <span class="price price_1">
-                        NT${{ msgCard.MSG_PRICE_1 }}
-                      </span>
-                    </div>
-                    <p class="rec rec_1">
-                      {{ msgCard.MSG_REC_1 }}
-                    </p>
-                  </div> -->
                   <div class="short_time">
                     <div class="short_time_price">
                       <span class="timespan timespan_2">
@@ -99,7 +86,7 @@ import { BASE_URL } from "@/assets/js/common.js";
 
 export default {
   props: {
-    msg: String,
+    // msg: String,
     msgCard: String,
   },
   data() {
@@ -115,13 +102,11 @@ export default {
       // var url = 'http://localhost/CGD102_G2/public/api/reservation.php' //開發用
       var url = `${BASE_URL}/reservation.php`; //上線用
       let getData = async (url) => {
-        // console.log(url);
         let response = await fetch(url); // await: 這行的 await 執行完才會執行下一個 await
         let JSON = response.json();
         this.msgCardList = await JSON; // php 抓取回來的資料存取在預設好的參數裡
       };
-      await getData(url); // 觸發 getData 的匿名 function 內容 ==> 89 ~ 91 行的內容
-      // console.log(this.msgCardList);
+      await getData(url); // 觸發 getData 的匿名 function 內容
     },
   },
 };

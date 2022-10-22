efe81497ecd8dfac87c13f1adfe961b5d1b78262
sandranii <niyouu63@gmail.com>
2022-10-22 22:23:39 +0800

    fix ResvPage v-if="msgData.length>0", del 多餘註解

--- src/components/ResvPage.vue
+++ src/components/ResvPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="resv_page">
     <main>
-      <section class="msg container">
+      <section class="msg container" v-if="msgData.length>0">
         <div class="topic">
           <div class="breadcrumb">
             <router-link to="/home">首頁</router-link> > <router-link to="/reservation">預約按摩</router-link> >
@@ -36,7 +36,6 @@
                     id="orderTherapist"
                     class="therapist"
                   >
-                    <!-- <option value="" disabled>請選擇按摩師傅</option> -->
                     <option value="1" selected>李駿燕 師傅</option>
                     <option value="2">言子倩 師傅</option>
                     <option value="3">康晉壕 師傅</option>
@@ -49,12 +48,6 @@
                     id="orderPrice"
                     class="timespan"
                   >
-                    <!-- <option disabled value="">請選擇時間長度</option> -->
-                    <!-- <option value="2400" >
-                        <span class="msg_timespan_1">120分鐘</span>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <span class="msg_price_1">NT$2400</span>
-                      </option> -->
                     <option :value="msgData[0].MSG_TIMESPAN_2" selected>
                       <span class="msg_timespan_1">{{ msgData[0].MSG_TIMESPAN_2 }}分鐘</span>
                       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -65,7 +58,6 @@
                   </select>
                   <p>請選擇預約日期及時間</p>
                   <DatePicker
-                    
                     v-model="valueTime"
                     type="datetime"
                     format="yyyy-MM-dd HH:mm"
@@ -117,7 +109,6 @@
               常見問題
             </button>
           </div>
-
           <div v-if="activeTab == 'A'" class="tab_container" v-html="msgData[0].MSG_DESC">
           </div>
           <div v-else-if="activeTab == 'B'" class="tab_container"
@@ -135,8 +126,6 @@
 </template>
 <script>
 import { DatePicker } from "view-ui-plus";
-// import zh from 'iview/dist/locale/zh-CN';
-// import 'view-ui-plus/dist/styles/viewuiplus.css'
 import { BASE_URL } from "@/assets/js/common.js";
 import { useRouter } from "vue-router";
 import lightBox from "@/components/lightBox.vue";
@@ -157,8 +146,6 @@ export default {
       isActive: true, //預測剛開始是打開active
       isActive2: false,
       isActive3: false,
-      orderList: [],
-      orderMsg: "全身穴道按摩",
       THERAPIST_ID: "1",
       orderPrice: "60",
       MSG_ID: "",
@@ -169,10 +156,6 @@ export default {
       valueTime: "10:00:00", //datepicker取出來的值
       timeResult: "", //轉換成想要的格式
       disableTimeArray: [["2022-10-10 10:00:00", "2022-10-13 12:00:00"]],
-      optionTime: {
-        disabledHours: "[1,5,10]",
-        disabledMinutes: "[0,10,20]",
-      },
     };
   },
   methods: {
@@ -194,25 +177,6 @@ export default {
       this.isActive2 = false;
       this.isActive3 = true;
     },
-    confirm() {
-      this.orderList.push({
-        orderMsg: "全身穴道按摩",
-        THERAPIST_ID: THERAPIST_ID.value,
-        orderPrice: orderPrice.value,
-      });
-      this.setStorage();
-      // console.log(this.timeResult);
-bmit()
-      window.alert("預約嗎？");
-    },
-    setStorage() {
-      localStorage.setItem("resvOrder", JSON.stringify(this.orderList));
-    },
-    getStorage() {
-      var orderLists = localStorage.getItem("resvOrder");
-      if (!orderLists) return;
-      this.orderList = JSON.parse(orderLists);
-    },
     submit() {
       if(!this.member){
          this.showBox = !this.showBox
@@ -228,39 +192,30 @@ bmit()
                 alert("預約成功");
                 // window.location.replace("/reservation"); //開發用
                 window.location.replace("/cgd102/g2/reservation"); //上線用
-                // this.router.push({ path: '/reservation'});
               }
             } else {
               alert(xhr.status);
             }
           };
-
           // let urlInsert = "http://localhost/CGD102_G2/public/api/resv.php";
           let urlInsert = `${BASE_URL}/resv.php`; //上線用
           xhr.open("post", urlInsert, true);
           xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-          // let resv_data = `RESV_DATE=${this.RESV_DATE}`;
           this.MEM_ID=`${this.member.memId}`;
-          // console.log('MEM_ID',this.MEM_ID);
           let resv_data = `MEM_ID=${this.MEM_ID}&THERAPIST_ID=${this.THERAPIST_ID}&MSG_ID=${this.MSG_ID}&RESV_DATE=${this.RESV_DATE}&RESV_TIME_START=${this.RESV_TIME_START}&RESV_TIME_END=${this.RESV_TIME_END}`;
-          console.log(resv_data);
           xhr.send(resv_data);
         }
       }
-   
     },
- 
   },
   created() {
     let members = sessionStorage.getItem("member");
     this.member = JSON.parse(members);
-    this.MSG_ID = this.$route.query.msg_id;
-  
     // 拿到從php傳回來的msg的data, id= 1,2,3...
-    this.getStorage();
+    this.MSG_ID = this.$route.query.msg_id;
+
     // let urlSelect = "http://localhost/CGD102_G2/public/api/msgPage.php"; //開發用
     let urlSelect = `${BASE_URL}/msgPage.php` //上線用
-
     this.axios
       .get(urlSelect, {
         params: {
@@ -268,40 +223,20 @@ bmit()
         },
       })
       .then((res) => {
-        // console.log(res.data);
-        // console.log(res.data[0]);
-        // console.log(res.data[0].MSG_NAME);
-        // console.log(res.data[0].MSG_NAME[0]);
-        // console.log(res.data[0].MSG_PIC);
         this.msgData = res.data;
         this.msgDataPic=this.msgData[0].MSG_PIC;
       });
-
-    // console.log(this.MSG_ID);
   },
   watch: {
-    orderList: {
-      handler(newVal, oldVal) {
-        this.setStorage(newVal);
-      },
-      deep: true, //監聽陣列內容
-    },
     valueTime(newVal) {
       let time = new Date(newVal);
       // 把datepicker取出來的值 轉換成想要的格式 2022-10-10 10:00:00
-
       this.timeResult = `${time.getFullYear()}-${
         time.getMonth() + 1
       }-${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
       this.RESV_DATE = `${time.getFullYear()}-${time.getMonth() + 1 <10?'0':''}${time.getMonth() + 1}-${time.getDate()<10?'0':''}${time.getDate()}`;
       this.RESV_TIME_START = `${time.getHours()}`;
       this.RESV_TIME_END = `${time.getHours() + 1}`; //只有1小時的時段可選擇就直接+1
-      // console.log(this.valueTime);
-      // console.log(this.timeResult);
-      // console.log(this.RESV_DATE);
-      // console.log(this.RESV_TIME_START);
-      // console.log(this.RESV_TIME_END);
-      // console.log("dd",dd);
     },
   },
   computed: {
